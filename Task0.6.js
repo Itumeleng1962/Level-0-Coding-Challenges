712da4a98516e31f444ba01b2ce70b307668e484
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-05-06 14:41:37 +0200

    Update Task0.6.js

--- Task0.6.js
+++ Task0.6.js
@@ -1,11 +1,11 @@
 function getMax(num1, num2, num3) {
-  if (num1 > num2 && num2 > num2) {
-    maxValue = num1;
-  } else if (num2 > num2) {
-    maxValue = num2;
-  } else {
-    maxValue = num3;
-    return maxValue;
+    if (num1 > num2 || num1 > num3) {
+      maxValue = num1;
+    } else if (num2 > num1 || num2 > num3) {
+      maxValue = num2;
+    } else if (num3 > num1 || num3>num2){
+      maxValue = num3;
+    }
+      return maxValue
   }
-}
-console.log(getMax());
+  console.log(getMax(10,50,36));
