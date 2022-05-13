ad4a6283185e64f6608647ae7077b4a71ecc2d4b
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-05-13 10:31:20 +0200

    Update Task0.5.js

--- Task0.5.js
+++ Task0.5.js
@@ -1,6 +1,6 @@
 function areaOfTriangle(side1, side2, side3) {
-  let semiParameter = (side1 + side2 + side3) / 2;
-  let area = Math.sqrt(semiParameter*(semiParameter-side1)*(semiParameter-side2)*(semiParameter-side3));
-  return area;
-}
-console.log(areaOfTriangle());
+    const semiParameter = (side1 + side2 + side3) / 2;
+    const area = Math.sqrt(semiParameter*(semiParameter-side1)*(semiParameter-side2)*(semiParameter-side3));
+    return area;
+  }
+  console.log(areaOfTriangle());
