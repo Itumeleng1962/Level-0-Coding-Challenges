79cf20b6b807c05f5dcef078d9630095fef5b3ff
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-05-14 17:27:03 +0200

    Update Task0.10.js

--- Task0.10.js
+++ Task0.10.js
@@ -1,14 +1,21 @@
 function getSameLetters(string1, string2) {
-const commonCharacters = "";
-  let sameLetter = "CommonLetters:";
-  for (let i = 0; i < string1.length; i++) {
-    if (sameLetter.indexOf(string1[i]) === -1) {
-      if (string2.indexOf(string1[i]) !== -1) {
-        sameLetter += string1[i];
+    let commonLetters = [];
+    for (let i = 0; i < string2.length; i++) {
+      for (let j = 0; j < string1.length; j++) {
+        if (string1[j].toLowerCase() === string2[i].toLowerCase()) {
+          commonLetters.push(string1[j].toLowerCase());
+        }
       }
     }
+    commonLetters = [...new Set(commonLetters)];
+  
+    let sameLetters = "Common letters: ";
+    for (let i = 0; i < commonLetters.length; i++) {
+      sameLetters += commonLetters[i];
+      if (i < commonLetters.length - 1) sameLetters += ", ";
+    }
+  
+    console.log(sameLetters);
   }
-  return sameLetter;
-};
 
-console.log(getSameLetters());
+getSameLetters(); 
