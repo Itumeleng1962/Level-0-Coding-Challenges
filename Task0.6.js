5dca6c652616d19604aa2694cc04244a033fb5e0
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-06-09 12:40:34 +0200

    Update Task0.6.js

--- Task0.6.js
+++ Task0.6.js
@@ -1,11 +1,11 @@
 function getMax(num1, num2, num3) {
-    if (num1 > num2 || num1 > num3) {
-      maxValue = num1;
-    } else if (num2 > num1 || num2 > num3) {
-      maxValue = num2;
-    } else if (num3 > num1 || num3>num2){
-      maxValue = num3;
-    }
-      return maxValue
-  }
-  console.log(getMax(10,50,36));
+   const a = [num1,num2,num3]
+    let maxValue = 0;
+    let answer = a.sort(function(a,b){
+        return b - a;
+    });
+
+    maxValue = answer[0];
+    return maxValue;
+}
+  console.log(getMax());
