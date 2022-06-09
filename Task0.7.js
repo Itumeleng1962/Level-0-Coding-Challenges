1c37ee6b9025d9fd4303398a8282929a51859d3d
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-06-09 12:48:28 +0200

    Update Task0.7.js

--- Task0.7.js
+++ Task0.7.js
@@ -1,7 +1,8 @@
 function fahrenheitToCelsius(fahrenheit) { 
-  return 1.8*(fahrenheit - 32) ; 
-}
-function celsiusToFahrenheit(celsius) {
-  return (celsius * 1.8) + 32; 
-}
-console.log(celsiusToFahrenheit(89));
+    return 1.8*(fahrenheit - 32) ; 
+  }
+  function celsiusToFahrenheit(celsius) {
+    return (celsius * 1.8) + 32; 
+  }
+  console.log(celsiusToFahrenheit(89));
+  console.log(fahrenheitToCelsius(32))
