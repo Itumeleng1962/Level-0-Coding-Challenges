7be156c498ece570fd330ad5d11a105ac39560e8
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-07-04 14:22:12 +0200

    Update Task0.4.js

--- Task0.4.js
+++ Task0.4.js
@@ -1,12 +1,9 @@
 function evenOrOdd(val) {
-   try{
-       if (val % 2 == 0) {
-        console.log("This number is even");
-         } else {
-          console.log("This number is odd");
-         }
-   }catch(err){
-       console.log(err);
-   }
-  }
-  evenOrOdd(3);
+      if (val % 2 == 0) {
+       console.log("This number is even");
+        } else {
+         console.log("This number is odd");
+        }
+
+ }
+ evenOrOdd(3)
