47fc0bb3c5c3f4dae95acb9bbeff8528322bda23
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-07-10 07:01:46 -0700

    Update Task0.9.js

--- Task0.9.js
+++ Task0.9.js
@@ -1,31 +1,40 @@
 function getVowels(name) {
-  let vowels = ["a", "e", "i", "o", "u"];
+  const vowels = ["a", "e", "i", "o", "u"];
+
   name = name.toLowerCase();
   let holdVowels = [];
+  let output = "Vowels: ";
+
   for (let i = 0; i < name.length; i++) {
     for (let j = 0; j < vowels.length; j++) {
       if (name[i] === vowels[j]) {
         holdVowels.push(vowels[j]);
+
+        for (let i = 0; i < holdVowels.length; i++) {
+          let countVowels = 0;
+          for (let j = 0; j < holdVowels.length; j++) {
+            if (holdVowels[i] == holdVowels[j]) {
+              countVowels++;
+            }
+            if (countVowels > 1) {
+              holdVowels.splice(j, 1);
+              j = j + 1;
+            }
+            
+          }
+         
+        }
+        
       }
     }
+    
   }
-  for (let i = 0; i < holdVowels.length; i++) {
-    let vowelCount = 0;
-    for (let j = 0; j < holdVowels.length; j++) {
-      if (holdVowels[i] == holdVowels[j]) {
-        vowelCount++;
-      }
-      if (vowelCount > 1) {
-        holdVowels.splice(j, 1);
-        j = j + 1;
-      }
-    }
-  }
-  let output = "Vowels: ";
+
   for (let i = 0; i < holdVowels.length; i++) {
     output += holdVowels[i];
     if (i < holdVowels.length - 1) output += ", ";
   }
   console.log(output);
+
 }
-getVowels("Umuzi")
+getVowels("Reneeeee")
